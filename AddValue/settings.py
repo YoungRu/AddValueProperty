bb778aa60d6af2e7c963a31f67b6f20e2f97ce44
YoungRu <young.loo2013@gmail.com>
2021-11-03 23:47:06 +0800

    postgres

--- AddValue/settings.py
+++ AddValue/settings.py
@@ -78,13 +78,15 @@ WSGI_APPLICATION = 'AddValue.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'addvalue',
+        'USER': 'postgres',
+        'PASSWORD': 'Young.loo1999',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
